40285d6e84b8dc16dd45631f2e8ac61b8105d780
Medhir Bhargava <medhirbhargava@gmail.com>
2015-06-15 13:26:44 -0700

    File changes, index page addition, css addition

---
+++ client/app.js
@@ -0,0 +1,39 @@
+/************************************************
+      Models
+*************************************************/
+
+var FoodItem = Backbone.Model.extend({
+
+});
+
+var SugarModel = Backbone.Model.extend({
+  
+});
+
+/************************************************
+      Collections
+*************************************************/
+
+var FoodItems = Backbone.Collection.extend({
+
+});
+
+/************************************************
+      Views
+*************************************************/
+
+var searchInput = Backbone.View.extend({
+
+});
+
+var FoodItemView = Backbone.View.extend({
+
+});
+
+var FoodItemsView = Backbone.View.extend({
+
+});
+
+var SugarView = Backbone.View.extend({
+
+});
---
+++ client/example.js
@@ -0,0 +1,22 @@
+/*
+Page for laying out all ideas and examples of code to create app
+
+-------------------------------------------------
+TODO
+-------------------------------------------------
+-handle API requests
+-create input field for making queries
+  ---> Backbone view that takes in input, has a submit event listener
+        -on submit, make a query, add items to Results collection
+-create backbone model for food item 
+  --->this will store all the relevant information about each food item
+-create backbone collection for food search results
+  --->this will store FoodItems
+
+-------------------------------------------------
+How to make an API request
+-------------------------------------------------
+
+Send a JSON object for the request, Content-Type header = "application/json"
+
+*/
